39284113fd451523f82f645ee24c24abfc2aec94
jd-smeltser <94378935+jd-smeltser@users.noreply.github.com>
2024-11-15 09:48:51 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tabflowy/',
+  base: './', // Change this from '/tabflowy/' to './'
   server: {
     port: 3000,
     open: true,
